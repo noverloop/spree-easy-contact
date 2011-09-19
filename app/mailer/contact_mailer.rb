bb228585b5a42adff9bd7b86eb69f3183042507c
noverloop <noverloop@gmail.com>
2011-09-20 00:21:07 +0200

    contact_email

--- app/mailer/contact_mailer.rb
+++ app/mailer/contact_mailer.rb
@@ -5,6 +5,6 @@ class ContactMailer < ActionMailer::Base
     subject = "#{Spree::Config[:site_name]} - #{t('message_from')} #{message.email}"
 
     @message = message
-    mail(:to => message.try(:topic).try(:email) , :subject => subject, :reply_to => message.email) 
+    mail(:to => Spree::Config[:contact_email] , :subject => subject, :reply_to => message.email) 
   end
 end
